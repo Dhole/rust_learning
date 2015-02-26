d05e463827e5ebb860a379c3714631acf7e3d728
Dhole <bankukur@gmail.com>
2015-02-26 19:30:45 +0100

    Added pop and len functions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 mod data_structures {
     use std::mem;
 
@@ -24,11 +23,20 @@ mod data_structures {
             let tmp_list = mem::replace(&mut *self.list, List::Nil);
             *self.list = List::Cons(elem, Box::new(tmp_list));
         }
-        /*
-           fn pop(&mut self) -> T {
+        pub fn pop(&mut self) -> Option<T> {
+            let tmp_list = mem::replace(&mut *self.list, List::Nil);
+            match tmp_list {
+                List::Cons(v, l) => {
+                    self.length -= 1;
+                    *self.list = *l; 
+                    Some(v) },
+                List::Nil => None,
+            }
+        }
+        pub fn len(&self) -> u32 {
+            self.length
+        }
 
-           }
-           */
         /*
         fn print(self) {
             match self.list {
@@ -42,12 +50,22 @@ mod data_structures {
 }
 
 use data_structures::LinkedList;
+use std::rand;
+use std::rand::Rng;
 
 fn main() {
     let mut l = LinkedList::<i32>::new();
+    let mut rng = rand::thread_rng();
+    for x in 0..100000 {
+        l.push(rng.gen());
+        //l.pop();
+    }
     l.push(1);
     l.push(2);
     l.push(3);
     //l.print();
+    println!("{} {} {}", l.pop().unwrap(), l.pop().unwrap(), 
+             l.pop().unwrap());
+    println!("Final lenght: {}", l.len());
     println!("Hello, world!");
 }
